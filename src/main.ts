cd1620fc5008f66a890c76f3019b49438ab91a0d
Alison Aguiar <alisonaguiar@MacBook-Pro-de-Alison.local>
2020-04-01 09:34:43 -0300

    comentario para teste de permissao no repositorio

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT') | 5000;
+  const port = configService.get('PORT') | 5000; //Comantario
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
